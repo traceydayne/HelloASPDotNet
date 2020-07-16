6685b0314c9ec98cfbfb4259de6eba5360ea34f8
Tracey Chandler <tracey.culhane@gmail.com>
2020-07-16 16:21:27 -0500

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+    }
+}
